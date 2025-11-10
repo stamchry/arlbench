aa81740f9aabe90644bc628fec139154586bcd03
Stam Chry <stamchry@gmail.com>
2025-11-10 12:55:29 +0100

    updated runscripts + cluster config

--- runscripts/smac/brax_ant_ppo.sh
+++ runscripts/smac/brax_ant_ppo.sh
@@ -4,15 +4,14 @@
 #SBATCH --cpus-per-task=4
 #SBATCH --mem-per-cpu=2000M
 #SBATCH --job-name=smac_brax_ant_ppo
-#SBATCH --account=p0021208
-#SBATCH --time 48:00:00                                 
+#SBATCH --time 01:00:00                                 
 #SBATCH --mail-type fail
-#SBATCH --mail-user dierkes.julian@rwth-aachen.de
+#SBATCH --mail-user stamatios.chrysanthidis@rwth-aachen.de
 #SBATCH --output smac_mf/log/smac_brax_ant_ppo_%A.out
 #SBATCH --error smac_mf/log/smac_brax_ant_ppo_%A.err
 #SBATCH --array 3-4%1
 
 cd ..
-source /rwthfs/rz/cluster/home/oh751555/i14/arlbench/.venv/bin/activate
-python runscripts/run_arlbench.py -m --config-name=tune_smac_mf experiments=brax_ant_ppo cluster=claix_gpu_h100 smac_seed=$SLURM_ARRAY_TASK_ID
+source /rwthfs/rz/cluster/home/aq055502/projects/arlbench-smac-hyper/arlbench/.venv/bin/activate
+python runscripts/run_arlbench.py -m --config-name=tune_smac_cost_aware_rf experiments=brax_ant_ppo cluster=claix_gpu_h100 smac_seed=$SLURM_ARRAY_TASK_ID
 
--- runscripts/smac_claix.sh
+++ runscripts/smac_claix.sh
@@ -10,19 +10,18 @@ mkdir -p "$directory/log"
 echo "#!/bin/bash
 
 
-#SBATCH --cpus-per-task=4
-#SBATCH --mem-per-cpu=2000M
+#SBATCH --cpus-per-task=2
+#SBATCH --mem-per-cpu=1000M
 #SBATCH --job-name=smac_${1}
-#SBATCH -t 08:00:00                                 
-#SBATCH --mail-type fail
+#SBATCH -t 01:00:00                                 
+#SBATCH --mail-type fail,end
 #SBATCH --mail-user stamatios.chrysanthidis@rwth-aachen.de
 #SBATCH --output $directory/log/smac_${1}_%A.out
 #SBATCH --error $directory/log/smac_${1}_%A.err
 #SBATCH --array 1,3
 
-cd ..
-source .venv/bin/activate
-python runscripts/run_arlbench.py -m --config-name=tune_smac_cost_aware_rf "experiments=$1" "cluster=$2" "smac_seed=\$SLURM_ARRAY_TASK_ID"
+source /rwthfs/rz/cluster/home/aq055502/projects/arlbench-smac-hyper/arlbench/.venv/bin/activate
+python runscripts/run_arlbench.py -m --config-name=tune_smac_cost_aware_rf experiments=$1 cluster=$2 smac_seed=\$SLURM_ARRAY_TASK_ID
 " > $directory/${1}.sh
 echo "Submitting $directory for $1"
 chmod +x $directory/${1}.sh
